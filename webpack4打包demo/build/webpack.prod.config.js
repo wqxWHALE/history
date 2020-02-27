e165c3de7257c19b3e361bbad58700cbbda97926
“wqxwqxWhale <844130791@qq.com>
2020-02-27 16:29:40 +0800

    delete

--- webpack4打包demo/build/webpack.prod.config.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const baseWebpackConfig=require('./webpack.base.config');
-
-exports.getProdConfigs=function(entryMap){
-    return merge(baseWebpackConfig, {
-        mode:"production",
-        entry:entryMap
-    });
-};
